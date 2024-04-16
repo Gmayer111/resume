6384f2b7604eb208bba9c8e08a850da629ed0698
Gmayer111 <gaelmayer@yahoo.fr>
2024-04-16 23:29:31 +0200

    fix: refacto resume page & add data

---
+++ src/data/dataResume/data.js
@@ -0,0 +1,28 @@
+const jobs = [
+  {
+    jobTitle: "Développeur Front-end",
+    companyName: "Curecall",
+    content:
+      "Projet de migration technologique : React vers Next.js et changement de version majeure de Next.js. Développement de packages npm et réutilisabilité du code",
+    startDate: "2022",
+    endDate: "2024",
+  },
+  {
+    jobTitle: "Développeur Back-end",
+    companyName: "Agence NWB",
+    content:
+      "Migration d’une ancienne version PHP du projet web pour un tour opérateur et développement de nouvelles fonctionnalités.",
+    startDate: "2022",
+    endDate: "2022",
+  },
+  {
+    jobTitle: "Forfaitiste",
+    companyName: "Tour opérateur Asia",
+    content:
+      "Réalisation de circuits sur-mesure et à la carte sur le continent asiatique et océanique.",
+    startDate: "2018",
+    endDate: "2022",
+  },
+];
+
+export default jobs;
--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -1,136 +1,83 @@
-import FlipPage from 'components/UI/FlipPage'
-import React from 'react'
-import 'assets/Styles/Resume.scss'
-import BlockResume from 'components/UI/BlockResume'
-import ResumeTitle from 'components/UI/ResumeTitle'
-import BlockMeasure from 'components/UI/BlockMeasure'
+import FlipPage from "components/UI/FlipPage";
+import React from "react";
+import "assets/Styles/Resume.scss";
+import BlockResume from "components/UI/BlockResume";
+import ResumeTitle from "components/UI/ResumeTitle";
+import BlockMeasure from "components/UI/BlockMeasure";
+import dataResume from "data/dataResume/data";
+import degrees from "data/dataDegrees/data";
+import technos from "data/dataTechnos/data";
 
 function Resume() {
-    return (
-        <FlipPage title={"Parcours."} logo={<i className="fas fa-university"></i>}>
-            <div className='main-block-resume'>
-                <div className='blockUp'>
-                    <div className="blockLeft">
-                        <div>
-                            <ResumeTitle title={'EXPERIENCE'} />
-                        </div>
-                        <div>
-                            <BlockResume 
-                                endDate={'2022'}
-                                startDate={'2022'}
-                                titleUp={'Développeur PHP junior'}
-                                titleDown={'Agence Web NWB'}
-                                content=
-                                {'Migration d’une ancienne version PHP du projet web pour un tour opérateur et développement de nouvelles fonctionnalités.'}
-                                height='230px'
-                                heightDate='255px'
-                            />
-                        </div>
-                        <div>
-                            <BlockResume 
-                                endDate={'2022'}
-                                startDate={'2018'}
-                                titleUp={'Forfaitiste'}
-                                titleDown={'Tour opérateur Asia'}
-                                content={'Réalisation de circuits sur-mesure et à la carte sur le continent asiatique et océanique.'}
-                                height='230px'
-                                heightDate='255px'
-                            />
-                        </div>
-                        <div>
-                            <BlockResume 
-                                endDate={'2018'}
-                                startDate={'2015'}
-                                titleUp={'Agent de voyages / Billettiste (plateau d\'affaires)'}
-                                titleDown={'Saint Léger Voyages & Allées Venues'}
-                                content={'Sociétés dans lesquelles j\'ai pû faire mon alternance, je m\'occupai de l\'achat de billets sur GDS pour une clientèle haut de gamme.'}
-                                height='290px'
-                                heightDate='315px'
-                            />                                
-                        </div>    
-                        <div>
-                            <BlockResume 
-                                endDate={'2015'}
-                                startDate={'2011'}
-                                titleUp={'Chef de rang / VVT Australie'}
-                                titleDown={'Elysée Lenôtre & Restaurant Australie'}
-                                content={'Durant ces années j\'ai pû mettre à profit mon expérience acquise dans des grandes brasseries parisiennes ainsi qu\'à l\'étranger.'}
-                                height='310px'
-                                heightDate='335px'
-                            />                                
-                        </div>
-                    </div>
-                    <div className="blockRight">
-                        <div>
-                            <ResumeTitle title={'DIPLOMES'} />
-                        </div>
-                        <div>
-                            <BlockResume 
-                                endDate={'2022'}
-                                startDate={'2021'}
-                                titleUp={`Graduate Développeur web full stack (BAC +2)`}
-                                titleDown={'Digital Campus (Studi)'}
-                                content={'Titre RNCP de niveau 5 reconnu par l\'état.'}
-                                height='200px'
-                                heightDate='225px'
-                            />
-                        </div>  
-                        <div>
-                            <BlockResume 
-                                endDate={'2018'}
-                                startDate={'2017'}
-                                titleUp={'Licence professionnelle Chef de projet touristique'}
-                                titleDown={'Paris School of Tourism & Communication'}
-                                content={'Diplômes reconnu par l\'état.'}
-                                height='360px'
-                                heightDate='385px'
-                            />
-                        </div>
-                    </div>
-                </div>
-                <div className="blockDown">
-                    <div className="blockDownLeft">
-                        <div className='block-title'>
-                            <ResumeTitle title={'FRONT-END'} />
-                        </div>
-                        <BlockMeasure 
-                            language={'HTML5'}
-                            size={'90'}
-                        />
-                        <BlockMeasure 
-                            language={'CSS / SASS'} 
-                            size={'60'}
-                        />
-                        <BlockMeasure 
-                            language={'Javascript - ECMAScript 6'} 
-                            size={'50'}
-                        />
-                        <BlockMeasure 
-                            language={'ReactJS'} 
-                            size={'50'}
-                        />
-                    </div>
-                    <div className="blockDownRight">
-                        <div className='block-title'>
-                            <ResumeTitle title={'BACK-END'} />
-                        </div>
-                        <BlockMeasure 
-                            language={'PHP'} 
-                            size={'50'}
-                        />
-                        <BlockMeasure 
-                            language={'MySQL'} 
-                            size={'60'}
-                        />
-                        <BlockMeasure 
-                            language={'Symfony'} 
-                            size={'50'}
-                        />
-                    </div>
-                </div>
+  return (
+    <FlipPage title={"Parcours."} logo={<i className="fas fa-university"></i>}>
+      <div className="main-block-resume">
+        <div className="blockUp">
+          <div className="blockLeft">
+            <div>
+              <ResumeTitle title={"EXPERIENCES"} />
             </div>
-        </FlipPage>
-    )
+
+            {dataResume.map((data) => (
+              <div>
+                <BlockResume
+                  titleUp={data.jobTitle}
+                  titleDown={data.companyName}
+                  content={data.content}
+                  startDate={data.startDate}
+                  endDate={data.endDate}
+                />
+              </div>
+            ))}
+          </div>
+          <div className="blockRight">
+            <div>
+              <ResumeTitle title={"DIPLOMES"} />
+            </div>
+            {degrees.map((data) => (
+              <div>
+                <BlockResume
+                  endDate={data.endDate}
+                  startDate={data.startDate}
+                  titleUp={data.diplomaTitle}
+                  titleDown={data.school}
+                  content={data.level}
+                  height="360px"
+                  heightDate="385px"
+                />
+              </div>
+            ))}
+          </div>
+        </div>
+
+        <div className="blockDown">
+          {technos.map((data) => (
+            <div
+              className={
+                data.title === "frontEnd" ? "blockDownLeft" : "blockDownRight"
+              }
+            >
+              <div className="block-title">
+                <ResumeTitle
+                  title={data.title === "frontEnd" ? "FRONT-END" : "OTHERS"}
+                />
+              </div>
+              {data.title === "frontEnd"
+                ? data.items.map((front) => (
+                    <BlockMeasure
+                      language={front.language}
+                      size={front.level}
+                    />
+                  ))
+                : data.items.map((back) => (
+                    <BlockMeasure language={back.language} size={back.level} />
+                  ))}
+            </div>
+          ))}
+        </div>
+      </div>
+    </FlipPage>
+  );
 }
 
-export default Resume
+export default Resume;
