2fc595d68e6225dcb2cfd21650453d4cf023e86a
Gmayer111 <gaelmayer@yahoo.fr>
2025-02-23 22:46:31 +0100

    fix: remove export to rootlayout font const

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,14 +3,14 @@ import "../../assets/sass/core.scss";
 import SideBar from "components/layout/sidebar.component";
 import { Playfair_Display, Roboto } from "next/font/google";
 
-export const roboto = Roboto({
+const roboto = Roboto({
   subsets: ["latin"],
   display: "swap",
   variable: "--font-roboto",
   weight: ["100", "300", "400", "500", "700", "900"],
 });
 
-export const playfair_Display = Playfair_Display({
+const playfair_Display = Playfair_Display({
   subsets: ["latin"],
   display: "swap",
   variable: "--font-playfair-display",
